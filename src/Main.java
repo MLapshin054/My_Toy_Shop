91bdc648e2fe4d9674dbae70d0c945aa34c68a01
Mikhail Lapshin <urbancell022@gmail.com>
2024-04-01 19:16:42 +0700

    add Toys.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        new Lottery();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Lottery.put("1 Doll 10 0.10");
+        Lottery.put("2 Car 15 0.15");
+        Lottery.put("3 Ball 7 0.07");
+        Lottery.put("4 Lego 5 0.06");
+        Lottery.put("5 Clock 3 0.04");
+        Lottery.put("6 Gun 20 0.20");
+        Lottery.put("7 Solder 8 0.08");
+        Lottery.put("8 Train 30 0.30");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        ArrayList<Toys> winners = new ArrayList<>();
+        
+
     }
 }

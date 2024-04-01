d1c4f44ccbc46c176c26c3ea5539587f44408532
Mikhail Lapshin <urbancell022@gmail.com>
2024-04-01 19:23:44 +0700

    modified Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,14 @@ public class Main {
         Lottery.put("8 Train 30 0.30");
 
         ArrayList<Toys> winners = new ArrayList<>();
-        
-
+        for (int i=0; i < 10; i++){
+            int wonToyId = Lottery.getToyId();
+            Toys wonToy = Lottery.getToyById(wonToyId);
+            assert wonToy != null;
+            if (Lottery.checkAvailable(wonToy)){
+                winners.add(wonToy);
+            }
+        }
+        Lottery.saveToFile("Result.txt", winners);
     }
 }
